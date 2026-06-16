e27fece931779ee7b485e56cc66a85100b61a3d7
Joey Lamping <35737718+jaylamping@users.noreply.github.com>
2026-06-16 02:03:59 -0500

    more quote

--- extension/sidepanel/quotes.js
+++ extension/sidepanel/quotes.js
@@ -11,16 +11,23 @@
     'WHAT THE FUCK RIGHT NOW?',
     'HURRY UP WITH MY DAMN CROISSANTS',
     'Poopity Scoop Whoopity Scoop',
-    "I'm nice at ping pong",
+    'I\u2019m nice at ping pong',
     'I leave my emojis Bart Simpson color',
-    "I have to dress Kim everyday so she doesn't embarrass me",
+    'I have to dress Kim everyday so she doesn\u2019t embarrass me',
     'I need a room full of mirrors so I can be surrounded by winners.',
     'Fur pillows are hard to actually sleep on',
-    "I no longer have a manager. I can't be managed",
+    'I no longer have a manager. I can\u2019t be managed',
     'Everybody knows the movie get out is about me',
     'To my fans\u2026 I can\u2019t finish the album because there\u2019s a bee in the studio',
     'Name one genius who ain\u2019t crazy',
-    'I like some of the Gaga songs, what the fuck she know about cameras?'
+    'I like some of the Gaga songs, what the fuck she know about cameras?',
+    'George Bush doesn\u2019t care about black people',
+    'You ain\u2019t got the answers, Sway!',
+    'I hate when I\u2019m on a plane and I wake up with a water bottle next to me like oh great now I gotta be responsible for this water bottle',
+    'My life is dope, and I do dope shit',
+    'Mayonnaise-colored Benz, I push Miracle Whips'
+
+
   ];
 
   root.FAA_randomQuote = function () {
