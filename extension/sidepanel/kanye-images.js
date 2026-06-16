3a0f1140071f5dc04be550bee469a1affd43ba2e
Joey Lamping <35737718+jaylamping@users.noreply.github.com>
2026-06-15 22:24:40 -0500

    Add Kanye portrait assets

--- extension/sidepanel/kanye-images.js
+++ extension/sidepanel/kanye-images.js
@@ -3,5 +3,16 @@
  * Source: extension/assets/kanye/
  */
 (function (root) {
-  root.FAA_KANYE_IMAGES = [];
+  root.FAA_KANYE_IMAGES = [
+  "1.jpg",
+  "10.jpg",
+  "2.jpg",
+  "3.jpg",
+  "4.jpg",
+  "5.jpg",
+  "6.jpg",
+  "7.jpg",
+  "8.jpg",
+  "9.jpg"
+];
 })(typeof window !== 'undefined' ? window : globalThis);
